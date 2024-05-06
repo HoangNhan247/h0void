a013fd32fda669d3e8be0bbf75da5c7fcafe26d7
HoangNhan247 <69146295+HoangNhan247@users.noreply.github.com>
2024-05-06 21:52:51 +0700

    C2.py

--- CuoiKi10/C2_Bug.py
+++ CuoiKi10/C2_Bug.py
@@ -1,4 +1,4 @@
-hovaten = str(input("Vui lòng nhập họ và tên đối tượng: ")).split(' ')
+hovaten = str(input("Vui lòng nhập họ và tên: ")).split(' ')
 
 print(hovaten)
 
@@ -10,5 +10,10 @@ if len(hovaten) > 2:
     for i in range(1, len(hovaten)-1):
 
         print(hovaten[i], end=' ')
+
+    print('\n Tên là: ', hovaten[len(hovaten)-1])
+
+else:
+    print('Họ là: ', hovaten[0])
 
     print('\n Tên là: ', hovaten[len(hovaten)-1])
