54852c29fe1f935a6529e94fa25c95a6a4e45a2a
HoangNhan247 <69146295+HoangNhan247@users.noreply.github.com>
2024-05-07 15:08:01 +0700

    Update C3.py

--- CuoiKi10/C3.py
+++ CuoiKi10/C3.py
@@ -1,15 +1,17 @@
-string = str(input("Nhập 1 xâu kí tự: "))
-
-num = 0
+string = input("Nhập 1 xâu kí tự: ") # Nhập xâu kí tự
+ 
+num = 0 # Khai báo biến số và kí tự để đếm
 char = 0
 
-for i in string:
+for i in string: # Vòng lặp duyệt từng phần tử ( i ) của string ( Xâu kí tự )
 
-    if '0' <= i <= '9':
-        num += 1
+    if '0' <= i <= '9': # Xét nếu kí tự i là kí tự kiểu chữ số
+        
+        num = num + 1 # Nếu thỏa mãn thì biến đếm số tăng 1
 
-    if 'A' <= i <= 'Z' or 'a' <= i <= 'z':
-        char += 1
+    if 'A' <= i <= 'Z' or 'a' <= i <= 'z': # Xét nếu kí tự i là kí tự kiểu chữ cái
+        
+        char += 1 # Nếu thỏa mãn thì biến đếm chữ tăng 1
 
-print('Trong xâu có tổng ký tự số là: ', num)
+print('Trong xâu có tổng ký tự số là: ', num) # In ra màn hình kết quả
 print('Trong xâu có tổng ký tự chữ là: ', char)
