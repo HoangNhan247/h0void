2fa890d4c7599e8fc1271f0cbc2337cb63316500
HoangNhan247 <69146295+HoangNhan247@users.noreply.github.com>
2024-05-07 14:59:42 +0700

    Update C1.py

--- CuoiKi10/C1.py
+++ CuoiKi10/C1.py
@@ -4,15 +4,15 @@ def Tach_day(xlist): # Khai báo hàm | xlist là tham số
     B = [] # Khai báo danh sách B và C
     C = []
 
-    for i in xlist: # Duyệt từng giá trị (Phần tử) của tham số
+    for i in range(xlist): # Đếm các chỉ số i từ 0 đến tổng số phần tử của danh sách xlist
 
-        if i % 2 == 0: # Xét giá trị tham số của danh sách
+        if i % 2 == 0: # Xét nếu chỉ số chẵn
 
             B.append(i) # Thêm giá trị i vào danh sách B nếu đạt điều kiện
 
-        elif i % 2 != 0: 
+        elif i % 2 != 0: # Xét nếu chỉ số lẻ
 
-            C.append(i)
+            C.append(i) # Thêm giá trị i vào danh sách C nếu đạt điều kiện
 
     print(B) # In ra danh sách B và C
     print(C)
