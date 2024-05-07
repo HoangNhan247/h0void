a9d0e356235007c4ccd78cabaa47973cfc73ff61
HoangNhan247 <69146295+HoangNhan247@users.noreply.github.com>
2024-05-07 14:46:32 +0700

    Update C1.py

--- CuoiKi10/C1.py
+++ CuoiKi10/C1.py
@@ -1,20 +1,20 @@
-A = [1, 2, 3, 4, 5, 6]
+A = [1, 2, 3, 4, 5, 6] # Danh sách
 
-def Tach_day(xlist):
-    B = [] # Khai báo danh sách B, C
+def Tach_day(xlist): # Khai báo hàm | xlist là tham số
+    B = [] # Khai báo danh sách B và C
     C = []
 
-    for i in xlist:
+    for i in xlist: # Duyệt từng giá trị (Phần tử) của tham số
 
-        if i % 2 == 0:
+        if i % 2 == 0: # Xét giá trị tham số của danh sách
 
-            B.append(i)
+            B.append(i) # Thêm giá trị i vào danh sách B nếu đạt điều kiện
 
-        elif i % 2 != 0:
+        elif i % 2 != 0: 
 
             C.append(i)
 
-    print(B)
+    print(B) # In ra danh sách B và C
     print(C)
 
-Tach_day(A)
+Tach_day(A) # Gọi hàm
