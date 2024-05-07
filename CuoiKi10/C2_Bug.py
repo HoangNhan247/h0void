6b9b7cbc9593566f489a02680447ed17df1ef131
HoangNhan247 <69146295+HoangNhan247@users.noreply.github.com>
2024-05-07 15:03:45 +0700

    Update C2_Bug.py

--- CuoiKi10/C2_Bug.py
+++ CuoiKi10/C2_Bug.py
@@ -1,19 +1,20 @@
-hovaten = str(input("Vui lòng nhập họ và tên: ")).split(' ')
+hovaten = input("Nhập họ và tên: ") # Nhập xâu kí tự họ và tên 
 
-print(hovaten)
+tachhovaten = hovaten.split(" ") # Tách các xâu kí tự trong hovaten qua kí tự dấu cách
 
-if len(hovaten) > 2:
-    print('Họ nạn nhân là: ', hovaten[0])
+if len(hovaten) > 2: # Xét nếu tổng phần tử trong hovaten lớn hơn 2
+    
+    print('Họ là: ', tachhovaten[0]) # In ra giá trị đầu của tachhovanten ( Họ )
 
-    print('Tên đệm là: ', end='')
-
-    for i in range(1, len(hovaten)-1):
-
+    print('Tên đệm là: ', end='') 
+    
+    for i in range(1, len(tachhovaten)-1): # In ra các phần tử xen kẽ giữa phần tử đầu và cuối của tachhovaten ( Tên đệm )
         print(hovaten[i], end=' ')
 
-    print('\n Tên là: ', hovaten[len(hovaten)-1])
+    print('\n Tên là: ', hovaten[len(hovaten)-1]) # In ra phần tử cuối của tachhovaten ( Tên )
 
-else:
-    print('Họ là: ', hovaten[0])
+else:  # Xét nếu tổng phần tử trong hovaten ít hơn 2
+    
+    print('Họ là: ', hovaten[0]) # In ra giá trị đầu của tachhovanten ( Họ )
 
-    print('\n Tên là: ', hovaten[len(hovaten)-1])
+    print('\n Tên là: ', hovaten[len(hovaten)-1]) # In ra phần tử cuối của tachhovaten ( Tên )
