5c6bfe9364a97c497aab2aadb490b671e10faf91
HoangNhan247 <69146295+HoangNhan247@users.noreply.github.com>
2024-05-09 15:28:39 +0700

    Update C2_Bug.py

--- CuoiKi10/C2_Bug.py
+++ CuoiKi10/C2_Bug.py
@@ -11,10 +11,10 @@ if len(hovaten) > 2: # Xét nếu tổng phần tử trong hovaten lớn hơn 2
     for i in range(1, len(tachhovaten)-1): # In ra các phần tử xen kẽ giữa phần tử đầu và cuối của tachhovaten ( Tên đệm )
         print(hovaten[i], end=' ')
 
-    print('\n Tên là: ', hovaten[len(hovaten)-1]) # In ra phần tử cuối của tachhovaten ( Tên )
+    print('\n Tên là: ', tachhovaten[len(tachhovaten)-1]) # In ra phần tử cuối của tachhovaten ( Tên )
 
 else:  # Xét nếu tổng phần tử trong hovaten ít hơn 2
     
-    print('Họ là: ', hovaten[0]) # In ra giá trị đầu của tachhovanten ( Họ )
+    print('Họ là: ', tachhovaten[0]) # In ra giá trị đầu của tachhovanten ( Họ )
 
-    print('\n Tên là: ', hovaten[len(hovaten)-1]) # In ra phần tử cuối của tachhovaten ( Tên )
+    print('\n Tên là: ', tachhovaten[len(hovaten)-1]) # In ra phần tử cuối của tachhovaten ( Tên )
