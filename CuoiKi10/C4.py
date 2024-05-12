8749d0bba9d8cc0f6120bc2f8e0dff8f51e818d0
HoangNhan247 <69146295+HoangNhan247@users.noreply.github.com>
2024-05-12 20:19:13 +0700

    Update C4.py

--- CuoiKi10/C4.py
+++ CuoiKi10/C4.py
@@ -1,16 +1,18 @@
-string = input("Nhập 1 xâu kí tự: ") # Khai báo xâu kí tự và kí tự cần tìm
+xaukitu = input("Nhập 1 xâu kí tự: ") # Khai báo xâu kí tự và kí tự cần tìm
 kitucantim = input("Nhập 1 kí tự cần tìm: ")
 
-def timkitu(str, char): # Khai báo hàm trong đó | str là tham số chỉ xâu | char là tham số chỉ kí tự
+def timkitu(xau, kitu): # Khai báo hàm trong đó | xau là tham số chỉ xâu | kitu là tham số chỉ kí tự
     
     xuathien = 0 # Khai báo biến đếm số lần kí tự xuất hiện
 
-    for i in str: # Duyệt từng kí tự trong xâu
+    for i in xau: # Duyệt từng kí tự trong tham số "xau"
 
-        if i == char: # Xét nếu kí tự trùng với kí tự cần tìm
+        if i == kitu: # Xét nếu kí tự trùng với tham số "kitu" ( kí tự cần tìm )
 
             xuathien = xuathien + 1 # Nếu thỏa mãn tăng giá trị đếm lên 1
 
     return xuathien # Trả lại giá trị xuất hiện
 
-print('Trong xâu ký tự đã xuất hiện', timkitu(string, kitucantim) ) # Gọi hàm trong print
+print('Trong xâu ký tự đã xuất hiện', timkitu(xaukitu, kitucantim) ) # Gọi hàm trong print
+
+# Nếu đề bài ra kí tự cần tìm có hơn 1 kí tự thì hết cứu :))
