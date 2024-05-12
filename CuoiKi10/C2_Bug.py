6fbfad1dac1ca57d19f47db12edbda89805079ba
HoangNhan247 <69146295+HoangNhan247@users.noreply.github.com>
2024-05-12 20:16:00 +0700

    Update C2_Bug.py

--- CuoiKi10/C2_Bug.py
+++ CuoiKi10/C2_Bug.py
@@ -1,20 +1,21 @@
 hovaten = input("Nhập họ và tên: ") # Nhập xâu kí tự họ và tên 
 
-tachhovaten = hovaten.split(" ") # Tách các xâu kí tự trong hovaten qua kí tự dấu cách
+tachhovaten = hovaten.split(" ") # Tách các xâu kí tự trong hovaten qua kí tự dấu cách thành danh sách
 
-if len(hovaten) > 2: # Xét nếu tổng phần tử trong hovaten lớn hơn 2
+ho = tachhovaten[0] # Lấy phần tử đầu của danh sách tachhovaten ( Họ )
+
+tendem = [] # Tạo 1 danh sách để chứa tên đệm
+
+ten = tachhovaten[len(tachhovaten)-1] # Lấy phần tử cuối của danh sách tachhovaten ( Tên )
+
+if len(tachhovaten) > 2: # Xét nếu tachhovaten có nhiểu hơn 2 phần tử ( Có cả họ, tên và tên đệm )
     
-    print('Họ là: ', tachhovaten[0]) # In ra giá trị đầu của tachhovanten ( Họ )
+    for i in range(1, len(tachhovaten)-1): # Lặp từ 1 đến số phần tử gần cuối của danh sách tachhovaten ( Giá trị xen kẽ giữa "họ" và "tên" )
+        
+        tendem.append(tachhovaten[i]) # Thêm tên đệm vào danh sách tên đệm
 
-    print('Tên đệm là: ', end='') 
-    
-    for i in range(1, len(tachhovaten)-1): # In ra các phần tử xen kẽ giữa phần tử đầu và cuối của tachhovaten ( Tên đệm )
-        print(hovaten[i], end=' ')
+print('Họ là:', ho) # In ra họ
+print('Tên đệm là:', " ".join(tendem)) # Sử dụng " ".join(tendem) để liên kết các phần tử trong danh sách tên đệm bằng dấu cách và in ra
+print('Tên là:', ten) # In ra tên
 
-    print('\n Tên là: ', tachhovaten[len(tachhovaten)-1]) # In ra phần tử cuối của tachhovaten ( Tên )
-
-else:  # Xét nếu tổng phần tử trong hovaten ít hơn 2
-    
-    print('Họ là: ', tachhovaten[0]) # In ra giá trị đầu của tachhovanten ( Họ )
-
-    print('\n Tên là: ', tachhovaten[len(hovaten)-1]) # In ra phần tử cuối của tachhovaten ( Tên )
+# Cảm ơn Lý Hà Minh Đức đã cho tui ý tưởng :3
